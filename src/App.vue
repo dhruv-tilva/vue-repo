ca83b66560c7c443631b3c525b44ff6c1c53b418
brijpatel2000 <tilavadhruv111@gmail.com>
2023-07-19 16:34:25 +0530

    Repo initialized

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <comp-one v-slot:firstSlot="receiveData">
+    <h1>{{ receiveData.firstName }}</h1>
+  </comp-one>
+
+  <comp-one v-slot:secondSlot="receiveData2">
+    <h1>{{ receiveData2.secondName }}</h1>
+  </comp-one>
+</template>
+
+<script>
+import CompOne from "./components/CompOne.vue"
+export default {
+  components: { CompOne }
+}
+</script>
---
+++ src/components/CompOne.vue
@@ -0,0 +1,15 @@
+<template>
+    <slot :firstName="data" name="firstSlot"></slot>
+    <slot :secondName="data2" name="secondSlot"></slot>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            data: "This is Data.",
+            data2: "This is Data2."
+        }
+    }
+}
+</script>
